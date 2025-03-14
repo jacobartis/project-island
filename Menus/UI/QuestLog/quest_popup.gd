56687bdd2f395c6130132e57488f8bafb690b9ab
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-03-14 12:58:13 +0000

    Updated quest popup to work

--- Menus/UI/QuestLog/quest_popup.gd
+++ Menus/UI/QuestLog/quest_popup.gd
@@ -4,7 +4,10 @@ extends Control
 func _on_quest_listener_quest_completed(id):
 	$QName.set_text(QuestManager.get_quest(id).quest_name)
 	$QName.self_modulate = Color.GREEN
-
+	$AnimationPlayer.play("fade")
+	$Completed.play()
 
 func _on_quest_listener_quest_started(id):
 	$QName.set_text(QuestManager.get_quest(id).quest_name)
+	$AnimationPlayer.play("fade")
+	$Started.play()
