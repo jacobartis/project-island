754a9ace15346e2a7c0a13d09c0339d5e53613e3
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-03-14 19:29:47 +0000

    New Models.

--- player/scenes/3rdPCam.gd
+++ player/scenes/3rdPCam.gd
@@ -1,12 +1,22 @@
 extends Node3D
 
 @export var cam: Camera3D
+@onready var spring_arm_3d = $SpringArm3D
 
+@export var transition_speed: float = 1
+@export var third_person_dist: float = 5
+var third: bool = true
 var sens: float = 0.01
 var limit = deg_to_rad(75)
 
 func _input(event):
 	move_cam(event as InputEventMouseMotion)
+
+func _process(delta):
+	if third:
+		spring_arm_3d.spring_length = lerpf(spring_arm_3d.spring_length,third_person_dist,transition_speed)
+	else:
+		spring_arm_3d.spring_length = lerpf(spring_arm_3d.spring_length,.01,transition_speed)
 
 func move_cam(event:InputEventMouseMotion):
 	if !event or not State.can_act(): return
@@ -15,3 +25,6 @@ func move_cam(event:InputEventMouseMotion):
 	rotation.x -= y*sens
 	rotation.x = clampf(rotation.x,-limit, limit)
 	rotation.y  -= x*sens
+
+func set_third_person(val:bool):
+	third = val
