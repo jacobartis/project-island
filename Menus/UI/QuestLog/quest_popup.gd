3cd02c56f519ce94320f5f428c1a1956303520ec
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-03-14 17:02:36 +0000

    Popup now looks correct when getting multiple quests quickly.

--- Menus/UI/QuestLog/quest_popup.gd
+++ Menus/UI/QuestLog/quest_popup.gd
@@ -4,10 +4,12 @@ extends Control
 func _on_quest_listener_quest_completed(id):
 	$QName.set_text(QuestManager.get_quest(id).quest_name)
 	$QName.self_modulate = Color.GREEN
+	$AnimationPlayer.stop()
 	$AnimationPlayer.play("fade")
 	$Completed.play()
 
 func _on_quest_listener_quest_started(id):
 	$QName.set_text(QuestManager.get_quest(id).quest_name)
+	$AnimationPlayer.stop()
 	$AnimationPlayer.play("fade")
 	$Started.play()
