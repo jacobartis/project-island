1fe78270f918067e4c3f757c6c5ff246ef26993f
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-03-15 22:58:28 +0000

    Resource for holding seed info.

---
+++ farming/crops/crop_entry.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name CropEntry
+
+
+@export var plant_name:String = "Placeholder"
+@export_multiline var description: String = "Placeholder description"
+
+@export var packed_plant: PackedScene = null
+@export var icon: Texture2D = null
