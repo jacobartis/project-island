cad1c2882220181d0a2cc745d0e66a239b267d80
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-03-15 19:48:30 +0000

    Fixed text not return to white

--- Menus/UI/QuestLog/quest_popup.gd
+++ Menus/UI/QuestLog/quest_popup.gd
@@ -10,6 +10,7 @@ func _on_quest_listener_quest_completed(id):
 
 func _on_quest_listener_quest_started(id):
 	$QName.set_text(QuestManager.get_quest(id).quest_name)
+	$QName.self_modulate = Color.WHITE
 	$AnimationPlayer.stop()
 	$AnimationPlayer.play("fade")
 	$Started.play()
