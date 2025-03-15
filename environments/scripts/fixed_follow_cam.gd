c1578a262371d904d025cd19cc390246dc797c95
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-03-15 23:02:41 +0000

    Bleah

--- environments/scripts/fixed_follow_cam.gd
+++ environments/scripts/fixed_follow_cam.gd
@@ -1,13 +1,26 @@
 extends Camera3D
+class_name FixedCamera
 
 @export var follow_player: bool = true
 
+var enabled: bool = true
+
 func _ready():
 	current = true
+	enabled = true
 
 func _process(delta):
+	if !enabled: return
 	var player = get_tree().get_first_node_in_group("player")
 	if !player: return
 	get_tree().get_first_node_in_group("player_cam").disable()
 	if follow_player:
 		look_at(player.global_position)
+
+func enable():
+	current = true
+	enabled = true
+
+func disable():
+	current = false
+	enabled = false
