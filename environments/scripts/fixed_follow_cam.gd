eb65d0b2004a8b118f22cea9c552ec0def130122
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-03-15 19:56:17 +0000

    Added toggle to follow.

--- environments/scripts/fixed_follow_cam.gd
+++ environments/scripts/fixed_follow_cam.gd
@@ -1,5 +1,7 @@
 extends Camera3D
 
+@export var follow_player: bool = true
+
 func _ready():
 	current = true
 
@@ -7,5 +9,5 @@ func _process(delta):
 	var player = get_tree().get_first_node_in_group("player")
 	if !player: return
 	get_tree().get_first_node_in_group("player_cam").disable()
-	
-	look_at(player.global_position)
+	if follow_player:
+		look_at(player.global_position)
