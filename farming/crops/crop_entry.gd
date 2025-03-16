25a745a3586589d8c55d4888fb402a3098ae2448
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-03-16 17:53:34 +0000

    Renamed scene.

--- farming/crops/crop_entry.gd
+++ farming/crops/crop_entry.gd
@@ -1,9 +1,8 @@
 extends Resource
 class_name CropEntry
 
-
 @export var plant_name:String = "Placeholder"
 @export_multiline var description: String = "Placeholder description"
 
-@export var packed_plant: PackedScene = null
+@export var scene: PackedScene = null
 @export var icon: Texture2D = null
