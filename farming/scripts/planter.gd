a5803003a404dbe38d53d1a5c3b3af0b919a284a
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-03-16 17:52:42 +0000

    Planter can now plant crops.

--- farming/scripts/planter.gd
+++ farming/scripts/planter.gd
@@ -1,17 +1,17 @@
 extends StaticBody3D
 
-@export var seed_holder:Node3D
+@export var crop_holder:Node3D
 
+var current_crop: Crop = null
 var farming: bool = false
 
 func enter_farming_mode():
 	get_tree().get_first_node_in_group("player_cam").disable()
 	if get_tree().get_first_node_in_group("room_cam"):
 		get_tree().get_first_node_in_group("room_cam").disable()
-	get_tree().get_first_node_in_group("planter_ui").show()
+	get_tree().get_first_node_in_group("planter_ui").display()
 	get_tree().get_first_node_in_group("planter_ui").current_planter = self
 	$Camera3D.current = true
-	State.in_menu = true
 	$Highlight.hide()
 	farming = true
 
@@ -20,12 +20,20 @@ func exit_farming_mode():
 	if get_tree().get_first_node_in_group("room_cam"):
 		get_tree().get_first_node_in_group("room_cam").enable()
 	get_tree().get_first_node_in_group("planter_ui").hide()
-	State.in_menu = false
 	$Highlight.show()
 	farming = false
 
-func plant_seed(seed:Crop):
-	pass
+func plant(crop_data:CropEntry):
+	if current_crop:
+		remove_current()
+	var crop = crop_data.scene.instantiate()
+	crop_holder.add_child(crop)
+	current_crop = crop
+	crop.owner = self
+
+func remove_current():
+	current_crop.queue_free()
+	current_crop = null
 
 func toggle_farming_mode():
 	print(farming)
