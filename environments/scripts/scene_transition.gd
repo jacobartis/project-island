00492be5edc2ff07d02ce11f8820bc831232563b
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-03-17 19:02:33 +0000

    Scene transtion now takes a scene name rather than file path.

--- environments/scripts/scene_transition.gd
+++ environments/scripts/scene_transition.gd
@@ -1,9 +1,9 @@
 extends Node
 class_name SceneTransition
 
-@export_file() var file:String
+@export var scene_name:String
 @export var exit_id:int = 0
 
 # TODO Find solution that save position
 func transition():
-	SceneHandler.transition(file,exit_id)
+	SceneHandler.transition(scene_name,exit_id)
